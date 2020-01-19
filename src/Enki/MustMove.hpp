2aa4c43b44ef3e7e8b2c13e0aa0c126c45adf51b
Zephilinox <Zephilinox@hotmail.co.uk>
2020-01-19 14:42:33 +0000

    Added MustMove & NonCopyable

---
+++ src/Enki/MustMove.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+//STD
+#include <type_traits>
+
+namespace enki {
+//Should be the default choice, particularly in non-generic or end-user code
+//Ensures at compile-time that a move is performed, and not a copy
+template <typename T>
+decltype(auto) mustMove(T&& t)
+{
+	constexpr bool is_const = std::is_const_v<std::remove_reference_t<T>>;
+	static_assert(!is_const,
+		"T is const and therefore can only be copied, not moved. Use std::move instead.");
+	return std::move(t);
+};
+}
---
+++ src/Enki/NonCopyable.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace enki
+{
+/*
+ * Deleting the copy constructor/assignment implicitly deletes the move constructor/assignment
+ * Declaring them to be = default has no affect on derived classes
+ * A derived class that requires move semantics must implement a move constructor/assignment manually
+ */
+class NonCopyableAndNonMovable
+{
+public:
+	inline constexpr NonCopyableAndNonMovable(const NonCopyableAndNonMovable&) noexcept = delete;
+	inline constexpr NonCopyableAndNonMovable& operator=(const NonCopyableAndNonMovable&) noexcept = delete;
+
+protected:
+	inline constexpr NonCopyableAndNonMovable() noexcept = default;
+	inline ~NonCopyableAndNonMovable() noexcept = default;
+};
+}
